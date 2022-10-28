e7f0dce1a63bfa41819f4b55b9d3decb18d03071
kbarker <kbarker.webdev@gmail.com>
2022-10-28 11:52:39 -0500

    debugging portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -53,6 +53,7 @@ const Portfolio = () => {
                                     </Link>
                                 </Fade>
                             </div>
+                                        {p.id}
                         </li>
                     );
                 })}
